0e6e63c18ee33db495ebee5a3bf9f122af18da21
Tushar V <tushar.vijayanagar@gmail.com>
2023-04-13 12:43:46 +0530

    added javascript

---
+++ birthday_indexer.js
@@ -0,0 +1,5 @@
+    function dummy() {
+        //   let xyz = document.getElementById("date_input").innerHTML
+        //alert(xyz) 
+        document.getElementById("example").innerHTML="<p style=\"font-size:20pt;color: white;\"> Mr._______ was born on your birthday too! <p>"
+    }
